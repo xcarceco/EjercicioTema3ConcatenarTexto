338b2cf0c340f2806770b6c14ec893fc139dacde
xcarceco <xcarceco@gmail.com>
2021-12-29 20:33:51 +0100

    Initial commit

---
+++ src/EjercicioBucle.java
@@ -0,0 +1,14 @@
+public class EjercicioBucle {
+
+
+        public static void main(String[] args) {
+            String [] nombres = {"Xavi", "Joan", "Antonio"};
+            for (int i = 0; i <=nombres.length; i++){
+                System.out.println(nombres[i]+nombres[i++]+nombres[i]);
+
+            }
+        }
+
+
+}
+
